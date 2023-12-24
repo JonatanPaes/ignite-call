b13cc6c199bddc1450f692d6ccd8ecba7b76247d
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-24 01:20:56 -0300

    feat: Utilizando React Hook Form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,28 @@
+import { useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
 import { Button, TextInput } from '@jonatanpaes-ui/react'
+import { z } from 'zod'
+
 import { Form } from './styles'
 
+const claimUsernameForm = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameForm>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {}
+
   return (
-    <Form as="form">
-      <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
 
       <Button size="sm" type="submit">
         Reservar
