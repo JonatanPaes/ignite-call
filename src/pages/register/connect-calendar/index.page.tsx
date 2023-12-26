70a7087b3ec0faee7df8e3dc21aac024d28df0ca
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-26 19:02:30 -0300

    feat: Configurando o Next Auth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,3 +1,4 @@
+import { signIn } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 import { Button, Heading, MultiStep, Text } from '@jonatanpaes-ui/react'
 
@@ -23,7 +24,11 @@ export default function Register() {
         <ConnectItem>
           <Text>Google Calendar</Text>
 
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
