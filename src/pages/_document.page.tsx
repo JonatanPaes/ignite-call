873996abf72c3cdde8c5000f0b74dbab159cffa2
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-01-10 21:38:40 -0300

    chore: add new favicon

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -17,6 +17,8 @@ export default function Document() {
           rel="stylesheet"
         />
 
+        <link rel="icon" href="/favicon.ico" />
+
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
